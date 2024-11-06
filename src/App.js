6bc7a8da65fd20b857c10bb9ad85e4d3a9460d04
Mikee Ojastro <mikeeojastro@gmail.com>
2024-11-06 18:11:01 +0800

    November 6, 2024

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Bank from './components/Bank/Bank';
+import Budget from './components/Budget/Budget';
 
 function App() {
   return (
     <div className="App">
       <Bank />
+      <Budget />
     </div>
   );
 }
